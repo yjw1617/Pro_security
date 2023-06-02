3b2ddbe501958ae027b7b89d5b54f8f0f3bbd040
yjw <1617324537@qq.com>
2023-06-02 21:43:41 +0800

    实现post请求成功

--- pro_app/cgi_proj/cgi_led.c
+++ pro_app/cgi_proj/cgi_led.c
@@ -11,9 +11,14 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <stdint.h>
+#define Json_Print(out, cmd, msg)  \
+    fprintf(out, "{\"cmd\":\"%d\",\"msg\":\"%s\"}", cmd, msg)
+
 int cgiMain(){
+    //返回http数据头
     //返回数据类型
-    fprintf(cgiOut, "Content-type: application/json\r\n\r\n");
+    cgiHeaderContentType("application/json");
+    // fprintf(cgiOut, "Content-type: application/json\r\n\r\n");
     // fprintf(cgiOut, data);
     // cgiFormString("led",msg_recv,1);
     char msg_send[128] = {0};
@@ -21,9 +26,20 @@ int cgiMain(){
         char* msg_recv = getenv("QUERY_STRING");
         char con[2] = {0};
         sscanf(msg_recv,"led=%s",con);
-        sprintf(msg_send, "{\"code\":\"%d\",\"msg\":\"%s\"}", 200, con);
-        fprintf(cgiOut, msg_send);
-    }else{
+        Json_Print(cgiOut, 0, getenv("REMOTE_ADDR"));
+        // fprintf(cgiOut, msg_send);
+    }else if(strcmp(getenv("REQUEST_METHOD"), "POST") == 0){
+        char cmd[4] = {0};
+        char con[1];
+        cgiFormResultType ret = cgiFormString("cmd", cmd, 4);
+        // if(cgiFormSuccess == ret){
+        //     Json_Print(cgiOut, 10, cmd);
+        // }
+
+        
+        // cgiFormString("led", con, 1);
+        
+        Json_Print(cgiOut, 1, cmd);
         // sprintf(msg_send, "{\"code\":\"%d\",\"msg\":\"%s\"}", 200, "strcmp error");
         // fprintf(cgiOut, msg_send);
     }
